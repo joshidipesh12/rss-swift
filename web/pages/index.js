8e9c51ffedc5e85c3208823c9cf3124ac4977728
Dipesh Joshi <joshidipesh12@gmail.com>
2022-10-16 20:02:51 +0530

    added prettier config

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,10 +1,10 @@
 import '@elastic/eui/dist/eui_theme_light.css';
-import { EuiProvider } from '@elastic/eui';
+import {EuiProvider} from '@elastic/eui';
 import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <EuiProvider colorMode='light'>
+    <EuiProvider colorMode="light">
       <Header />
     </EuiProvider>
   );
