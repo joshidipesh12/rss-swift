349c6bf1901d5d6c8d13bbba04d2b0ec6a878d03
Darshita Crap-Bag <115578749+darshita10308@users.noreply.github.com>
2022-10-19 12:52:50 +0530

    stripped all theming logic from redux

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,50 +1,19 @@
 import '../styles/globals.css';
-import {useEffect, useLayoutEffect, useState} from 'react';
-
 import '@elastic/eui/dist/eui_theme_light.css';
+
 import {EuiProvider} from '@elastic/eui';
+import {Provider} from 'react-redux';
 
 import store from '../store';
-import {Provider} from 'react-redux';
-import {useDispatch, useSelector} from 'react-redux';
-
-import {syncTheme} from '../store/slices/theme';
-import {themeSelector} from '../store/selectors';
-import useThemeDetector from '../hooks/useThemeDetector';
 
 function MyApp({Component, pageProps}) {
-  const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    setShow(true);
-  }, []);
-
-  return show ? (
-    <Provider store={store}>
-      <MyAppWithRedux Component={Component} pageProps={pageProps} />
-    </Provider>
-  ) : null;
-}
-
-function MyAppWithRedux({Component, pageProps}) {
-  const dispatch = useDispatch();
-  const theme = useSelector(themeSelector); // redux theme
-  const system = useThemeDetector();
-
-  useLayoutEffect(() => {
-    dispatch(syncTheme());
-    return () => {};
-  }, []);
-
-  useEffect(() => {
-    console.table({theme, system});
-  });
-
-  return (
-    <EuiProvider colorMode={theme}>
-      <Component {...pageProps} />
-    </EuiProvider>
-  );
+    return (
+      <Provider store={store}>
+        <EuiProvider colorMode="LIGHT">
+          <Component {...pageProps} />
+        </EuiProvider>
+      </Provider>
+    );
 }
 
 export default MyApp;
