e6b74b222e398573953b31e1d07b39231b843604
Dipesh Joshi <joshidipesh12@gmail.com>
2022-11-17 17:15:04 +0530

    init

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -8,10 +8,7 @@ export default function Document() {
           href='https://api.fontshare.com/v2/css?f[]=chillax@600,500,300,400,200&display=swap'
           rel='stylesheet'></link>
         <link rel='preconnect' href='https://fonts.googleapis.com'></link>
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin></link>
+        <link rel='preconnect' href='https://fonts.gstatic.com'></link>
         <link
           href='https://fonts.googleapis.com/css2?family=Anton&display=swap'
           rel='stylesheet'></link>
